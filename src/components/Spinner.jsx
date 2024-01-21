fa24f19952a3089e6a4b2f1e34022f41bc680251
virendersiddharth <virendersiddharth24@gmail.com>
2024-01-21 15:13:18 +0530

    first commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className='w-full min-h-screen grid place-content-center'>
+      <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
